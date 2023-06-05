c5c67e2f61be916a7925ec4e069be84975cf15f2
ppwner <135299606+ppwner@users.noreply.github.com>
2023-06-04 22:06:52 -0400

    Update Bypass.cs

--- Bypass.cs
+++ Bypass.cs
@@ -1,29 +1,35 @@
 using System; using System.Net.Http; using HtmlAgilityPack; using System.Threading.Tasks;
 
-class P
+class Program
 {
-    static readonly HttpClient c = new HttpClient();
-    
+    static readonly HttpClient client = new HttpClient();
+
     static async Task Main()
     {
         Console.Write("Enter your HWID: ");
-        string h = Console.ReadLine();
-        var s = new[] { $"https://flux.li/windows/start.php?updated_browser=true&HWID={h}", "https://fluxteam.net/windows/checkpoint/check1.php", "https://fluxteam.net/windows/checkpoint/check2.php", "https://fluxteam.net/windows/checkpoint/main.php" };
-        
-        foreach (var x in s) { await R(x); Console.WriteLine($"\nBypassed {x}"); }
-        
-        var r = await R(s[^1]);
-        var d = new HtmlDocument();
-        d.LoadHtml(await r.Content.ReadAsStringAsync());
-        var k = d.DocumentNode.SelectSingleNode("//main/code[2]")?.InnerText.Trim();
-        
-        Console.WriteLine("\nYour key is: " + (k ?? "(unknown error)"));
-        Console.WriteLine("Press any key to exit..."); Console.ReadKey();
+        string hardwareId = Console.ReadLine();
+        var urls = new[] {$"https://flux.li/windows/start.php?updated_browser=true&HWID={hardwareId}","https://fluxteam.net/windows/checkpoint/check1.php","https://fluxteam.net/windows/checkpoint/check2.php","https://fluxteam.net/windows/checkpoint/main.php"};
+
+        foreach (var url in urls)
+        {
+            await Request(url);
+            Console.WriteLine($"\nBypassed {url}");
+        }
+
+        var response = await Request(urls[^1]);
+        var document = new HtmlDocument();
+        document.LoadHtml(await response.Content.ReadAsStringAsync());
+        var key = document.DocumentNode.SelectSingleNode("//main/code[2]")?.InnerText.Trim();
+
+        Console.WriteLine("\nYour key is: " + (key ?? "(unknown error)"));
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
     }
-    
-    static async Task<HttpResponseMessage> R(string u)
+
+    static async Task<HttpResponseMessage> Request(string url)
     {
-        var r = new HttpRequestMessage(HttpMethod.Get, u); r.Headers.Add("Referer", "https://linkvertise.com/");
-        return await c.SendAsync(r);
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+        request.Headers.Add("Referer", "https://linkvertise.com/");
+        return await client.SendAsync(request);
     }
 }
