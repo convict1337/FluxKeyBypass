11a7146a40ee4a3cb0656c717e9caf3cd796f15f
ppwner <135299606+ppwner@users.noreply.github.com>
2023-06-08 00:45:15 -0400

    Update Bypass.cs
    unpatched

--- Bypass.cs
+++ Bypass.cs
@@ -1,36 +1,35 @@
-using System; using System.Net.Http; using HtmlAgilityPack; using System.Threading.Tasks;
+using HtmlAgilityPack; using System; using System.Linq; using System.Net.Http; using System.Threading.Tasks;
 
 class Program
 {
-    static readonly HttpClient client = new HttpClient();
+    private static readonly HttpClient Client = new HttpClient();
 
     static async Task Main()
     {
         Console.Write("Enter your HWID: ");
-        string hardwareId = Console.ReadLine();
-        var urls = new[] {$"https://flux.li/windows/start.php?updated_browser=true&HWID={hardwareId}","https://fluxteam.net/windows/checkpoint/check1.php","https://fluxteam.net/windows/checkpoint/check2.php","https://fluxteam.net/windows/checkpoint/main.php"};
-
-        foreach (var url in urls)
-        {
-            await Request(url);
-            Console.WriteLine($"\nBypassed {url}");
-            await Task.Delay(1000);
-        }
-
-        var response = await Request(urls[^1]);
+        string hwid = Console.ReadLine();
+        var urls = new[] { $"https://flux.li/windows/start.php?updated_browser=true&HWID={hwid}", "https://fluxteam.net/windows/checkpoint/check1.php", "https://fluxteam.net/windows/checkpoint/check2.php", "https://fluxteam.net/windows/checkpoint/main.php" };
+        var responsetasks = urls.Select(Request);
         var document = new HtmlDocument();
-        document.LoadHtml(await response.Content.ReadAsStringAsync());
-        var key = document.DocumentNode.SelectSingleNode("//main/code[2]")?.InnerText.Trim();
-
-        Console.WriteLine("\nYour key is: " + (key ?? "(unknown error)"));
-        Console.WriteLine("Press any key to exit...");
+        foreach (var responsetask in responsetasks)
+        Console.WriteLine($"Bypassed {(await responsetask).Url}\n");
+        document.LoadHtml((await Request(urls.Last())).Content);
+        Console.WriteLine($"\nYour key is: {document.DocumentNode.SelectSingleNode("//main/code[2]")?.InnerText.Trim() ?? "(unknown error)"}\nPress any key to exit...");
         Console.ReadKey();
     }
 
-    static async Task<HttpResponseMessage> Request(string url)
+    static async Task<Response> Request(string url)
     {
-        var request = new HttpRequestMessage(HttpMethod.Get, url);
-        request.Headers.Add("Referer", "https://linkvertise.com/");
-        return await client.SendAsync(request);
+        Client.DefaultRequestHeaders.Clear();
+        Client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36");
+        Client.DefaultRequestHeaders.Referrer = new Uri("https://linkvertise.com/");
+        return new Response(url, await Client.GetStringAsync(url));
+    }
+
+    class Response
+    {
+        public string Url { get; }
+        public string Content { get; }
+        public Response(string url, string content) => (Url, Content) = (url, content);
     }
 }
